8df0170b5c0449a9008733940a82ff024bacc8b4
darlingson <dmakuwila@gmail.com>
2025-07-13 19:05:01 +0200

    project init

---
+++ src/test/java/com/darlingson/OIDC_Server/OidcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darlingson.OIDC_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
